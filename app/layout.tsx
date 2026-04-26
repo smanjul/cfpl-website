09ac7d6caca65cde52e2aa2760befcbffd2b8661
Manjul Sharma <67830013+smanjul@users.noreply.github.com>
2026-04-26 15:18:47 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,18 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Navbar from "@/components/Navbar";
 import "./globals.css";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="min-h-full flex flex-col">
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
